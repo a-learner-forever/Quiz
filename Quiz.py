5e0d8edc40f695cc5622f6cbd9328be271d2a101
Asmita <64234753+a-learner-forever@users.noreply.github.com>
2025-07-30 07:34:09 +0530

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,13 @@
+print("*** Welcome to the or quiz created in Python ***")
+
+print("How many continents are there in the world?")
+print("Press 1 if you say 6")
+print("Press 2 if you say 7")
+print("Press 3 if you say 8")
+
+choice = input("Enter 1, 2, or 3:")
+
+if choice == "2":
+   print("That's the correct answer.")
+else:
+	print("Incorrect option.")
